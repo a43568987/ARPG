3c9a62e58124568c7fc52ab75e9b013f6184195a
Alutemurat <48148353+Alutemurat@users.noreply.github.com>
2019-03-04 19:01:35 +0800

    游戏开始页面，写在GameEntry里
    添加了两个脚本，分别是LogoControl和StartGame，放在Framework里，分别控制logo的渐变和开始按钮的场景跳转和呼吸效果

---
+++ Client/Diablo/Assets/Scripts/Framework/StartGame.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 这是控制Start按钮的脚本，挂在GameEntry上
+/// </summary>
+namespace Framework
+{
+    public class StartGame
+    {
+        public Button c_StartButton;//按钮
+        public CanvasGroup c_StartGame;//使用CanvasGroup实现按钮的呼吸效果
+        private float c_FlashSpeed = 1.0f;//速度
+        private bool m_Flash;
+
+
+        void Start()
+        {
+            c_StartGame.alpha = 0;
+            m_Flash = false;
+        }
+
+        void Update()
+        {
+            if (c_StartButton.IsActive())
+            {
+                ButttonFlash();
+            }
+        }
+
+        public void StartButton()//场景跳转
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        public void ButttonFlash()//实现按钮呼吸效果
+        {
+            if (!m_Flash)
+            {
+                c_StartGame.alpha  += c_FlashSpeed * Time.deltaTime;
+                if (c_StartGame.alpha == 1)
+                {
+                    m_Flash = true;
+                }
+            }
+            else
+            {
+                c_StartGame.alpha -= c_FlashSpeed * Time.deltaTime;
+                if (c_StartGame.alpha == 0)
+                {
+                    m_Flash = false;
+                }
+            }
+        }
+    }
+}
+
